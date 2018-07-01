7971af0343ca4e0a6ddb4ff945847d8c1cf1b8ff
okalintu <ilkka.oks4nen@gmail.com>
2018-07-01 19:11:22 +0300

    Implement tgapi basics

---
+++ tgapi.py
@@ -0,0 +1,63 @@
+import requests
+
+class IncomingTelegramCommand:
+    def __init__(self, msg_id : int, sender_id : int, chat_id: int, command : str):
+        self.msg_id = msg_id
+        self.sender_id = sender_id
+        self.chat_id = chat_id
+        self.command = command
+
+    @classmethod
+    def from_tg_dict(cls, item : dict):
+        msg = item['message']
+        data = {
+            'msg_id': msg['message_id'],
+            'sender_id': msg['from']['id'],
+            'chat_id': msg['chat']['id'],
+            'command': msg['text'],
+        }
+        return cls(**data)
+
+    def __str__(self):
+        return f'Chat: {self.chat_id}, Command: {self.command}'
+
+class BaseCommandHandler:
+    command = '/hello'
+
+    def __init__(self):
+        with open('tgapi.key') as f:
+            token = f.read().strip()
+        self.send_message_url = f'https://api.telegram.org/bot{token}/sendMessage'
+
+    def match(self, message : IncomingTelegramCommand) -> bool:
+        '''
+        Return true if this handler should handle command. 
+        False otherwise
+        '''
+        return message.command.startswith(self.command)
+
+    def handle(self, message : IncomingTelegramCommand):
+        self._send_message(message.chat_id, 'Hello!', message.msg_id)
+
+    def _send_message(self, chat_id : int, message : str, message_id : int):
+        data = {
+            'chat_id': chat_id,
+            'text': message, 
+        }
+        requests.post(self.send_message_url, json=data)
+
+class CommandDelegator:
+    def __init__(self):
+        self.handlers = {}
+    
+    def register_handler(self, handler_id : str, handler : BaseCommandHandler) -> None:
+        self.handlers[handler_id] = handler
+    
+    def delete_handler(self, handler_id :str) -> None:
+        if handler_id in self.handlers:
+            del self.handlers[handler_id]
+
+    def delegate_command(self, message: IncomingTelegramCommand):
+        for _, handler in self.handlers.items():
+            if handler.match(message):
+                handler.handle(message)
---
+++ tgbot.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+from tgapi import IncomingTelegramCommand, CommandDelegator, BaseCommandHandler
+
+app = Flask(__name__)
+
+command_delegator = CommandDelegator()
+command_delegator.register_handler("HelloHandler", BaseCommandHandler())
+
+@app.route('/tgbot/')
+def main():
+    return '<h1>Blip!</h1>'
+
+@app.route('/tgbot/xWunSf50L2sRTjAIMd8I/', methods=['POST'])
+def botmain():
+    raw_data = request.get_json()
+    cmd = IncomingTelegramCommand.from_tg_dict(raw_data)
+    command_delegator.delegate_command(cmd)
+    return ""
