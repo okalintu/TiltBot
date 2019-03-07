50c747603475b277d14d91ccefb0bfb3cfa81f35
okalintu <ilkka.oks4nen@gmail.com>
2019-03-07 15:52:33 +0200

    Expose the flask only to the localhost by default

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 docker run --rm -d \
            -e FLASK_APP=tgbot.py \
            -e FLASK_ENV=development \
-           -p 5000:5000 \
+           -p 127.0.0.1:5000:5000 \
            -v /var/run/redis/redis.sock:/tmp/redis.sock \
            -v $DIR/keys/:/keys \
            -v $DIR/:/code \
