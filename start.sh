04d73a04dc44925764ef03990772f351f47958cd
Ilkka Oksanen <ilkka@ioks.eu>
2019-09-15 19:29:38 +0300

    Add restart to start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-docker run --rm -d \
+docker run -d \
            -e FLASK_APP=tgbot.py \
            -e FLASK_ENV=development \
            -p 127.0.0.1:5000:5000 \
            -v /var/run/redis/redis.sock:/tmp/redis.sock \
            -v $DIR/keys/:/keys \
            -v $DIR/:/code \
-           -l tiltbot tiltbot
+           --restart=always \
+           --name tiltbot tiltbot
